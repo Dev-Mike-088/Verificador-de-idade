16de75f7646dc50d5a3d013b5993f7757cae09e3
Dev-Mike-088 <mikaeleal28@gmail.com>
2025-02-24 21:00:53 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class ageVerification{
+    static void Main(){
+        Console.WriteLine("Me informe sua Idade :");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age <= 0){
+            Console.WriteLine("Por favor informe um Número válido !!");
+        }else if(age < 18){
+            Console.WriteLine("Você é de Menor");
+        }else{
+            Console.WriteLine("Você é de Maior");
+        }
+    }
+}
